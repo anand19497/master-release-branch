6f06f57ec44e4bdafd102af48ab703d304d48e18
anand19497 <princemishra19497@gmail.com>
2024-10-06 02:24:10 +0530

    springboot Restcurdapi RESTPOJO RESTpathvariable

---
+++ SpringBoot/RestCurdApi/03-springboot-restcurd/demoforRestcurd/src/main/java/com/Anandcode/demoforRestcurd/rest/DemoforRestcurdApplication.java
@@ -0,0 +1,13 @@
+package com.Anandcode.demoforRestcurd.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoforRestcurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoforRestcurdApplication.class, args);
+	}
+
+}
